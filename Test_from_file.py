f80232089032831db516a51b1941f7560f089b54
EmiTCDC <101722438+EmiTCDC@users.noreply.github.com>
2022-05-16 13:17:24 +0200

    Test_from_file: Adapted for easy run (Temporary)

--- Test_from_file.py
+++ Test_from_file.py
@@ -1,52 +1,79 @@
-import warnings
+# Import class
 from Scenario_ConstellationDeployment import Scenario
+
+# Import libraries
+import warnings
 import sys
 from json import load as load_json
 import os
 warnings.filterwarnings("ignore")
 
-config_file = sys.argv[1]
+# Set configuration file as input or manually inserted
+config_file = "test_config.json" #= sys.argv[1]
 
-# read properties from json config file
+# Open .json and read mission description
 with open(config_file) as file:
+    # Open file
     json = load_json(file)
+    
+    # Extract output folder path
     results_folder_path = json["data_path"]
 
-# create the Results folder if it does not exist
+# Create results folder if non-existent
 if not os.path.exists(results_folder_path):
     os.makedirs(results_folder_path, exist_ok=True)
 
-# this bool enables/disables the generation of txt files and restore the console printing. For debugging purposes
+# DEBUG: Toggling bool for console printing: True = Print in file | False = print in console
 print_to_files = True
+
+# Check for debug
 if print_to_files:
+    # Access system output and error
     original_stdout = sys.stdout  # Save a reference to the original standard output
     original_stderr = sys.stderr  # Save a reference to the original standard error
 
-    # open files (with statement wont write exceptions to the file, see https://stackoverflow.com/questions/66151573)
-    # also, we open files with line-buffering mode (buffering=1) for flushing more often (every line)
+    # Open files (with statement wont write exceptions to the file, see https://stackoverflow.com/questions/66151573)
+    # Files open with line-buffering mode (buffering=1) for flushing more often (every line)
     result = open(os.path.join(results_folder_path, 'result.txt'), 'w', 1, encoding="utf-8")
     log = open(os.path.join(results_folder_path, 'log.txt'), 'w', 1, encoding="utf-8")
-
+    
+    # Link output to .txt files
     sys.stdout = result  # Change the standard output to the file result.txt
     sys.stderr = log  # Change the standard error to the file log.txt
 
+    # Instanciate scenario object
     scenario = Scenario("test_from_file", config_file)
+    
+    # Set-up scenario
     scenario.setup()
+    
+    # Execute scenario
     results = scenario.execute()
-
+    
+    # Print scenario reports
     scenario.plan.print_report()
     scenario.fleet.print_report()
-
-    sys.stdout = original_stdout  # Reset the standard output to its original value
-    sys.stderr = original_stderr  # Reset the standard error to its original value
-
+    
+    # Close .txt file
     result.close()
     log.close()
+    
+    # Reset output to initial value
+    sys.stdout = original_stdout  # Reset the standard output to its original value
+    sys.stderr = original_stderr  # Reset the standard error to its original value
+    
+    
 else:
+    # Instanciate scenario object
     scenario = Scenario("test_from_file", config_file)
+    
+    # Set-up scenario
     scenario.setup()
+    
+    # Execute scenario
     results = scenario.execute()
 
+    # Print scenario reports
     scenario.plan.print_report()
     scenario.fleet.print_report()
 
