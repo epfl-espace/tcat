8ede019c0a21eabe040721ccbe98ef2e27844f50
malogoury-tcdc <102596835+malogoury-tcdc@users.noreply.github.com>
2022-06-02 12:46:26 +0200

    ScenarionParameters: renamed file

--- Scenario/ScenarioParameters.py
+++ Scenario/ScenarioParameters.py
@@ -18,5 +18,8 @@ INSERTION_RAAN_WINDOW = 5 * u.deg # Defines the admissible range of RAAN around 
 # The models include contingencies to account for model errors:
 CONTINGENCY_DELTA_V = 0.1 # dV added to computed dV for each manoeuvres
 
-# different models exist for a same type of manoeuvre, selection is based on certain criteria:
+# Different models exist for a same type of manoeuvre, selection is based on certain criteria:
 MODEL_RAAN_DIRECT_LIMIT = 8 * u.deg # criteria to select between direct RAAN change (small angles only) or use J2 model
+
+# Define the input json name and path here (can also use argv[1])
+SCENARIO_INPUT_JSON = "Constellation_new_v1.json"
