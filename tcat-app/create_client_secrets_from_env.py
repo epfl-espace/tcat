cf6780f486175720ff11806ad395c7542573c772
Orell Bühler <orell.buehler@ateleris.ch>
2022-06-10 09:52:25 +0200

    feat: create script to generate client secrets from env

---
+++ tcat-app/create_client_secrets_from_env.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import json
+
+web_data = {'issuer': os.getenv('IDP_ISSUER'), 'auth_uri': os.getenv('IDP_AUTH_URI'),
+            'client_id': os.getenv('IDP_CLIENT_ID'), 'client_secret': os.getenv('IDP_CLIENT_SECRET'),
+            'redirect_uris': [os.getenv('IDP_REDIRECT_URI')], 'userinfo_uri': os.getenv('IDP_USERINFO_URI'),
+            'token_uri': os.getenv('IDP_TOKEN_URI'), 'token_introspection_uri': os.getenv('IDP_INTROSPECTION_URI')}
+
+data = {'web': web_data}
+json_data = json.dumps(data)
+
+f = open(sys.argv[1], "w")
+f.write(json_data)
+f.close()
