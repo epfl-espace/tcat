ff31f56092e07ceafd0fbede6192c94e05953b5f
MatUspace <93520071+MatUspace@users.noreply.github.com>
2022-06-22 15:24:06 +0200

    Update inputparams.py
    Remove launch_vehicle architecture option
    Put again sat_volume default to None with min volume to 0.001 m^3

--- tcat-app/inputparams.py
+++ tcat-app/inputparams.py
@@ -1,7 +1,7 @@
 # control-datatype-lookup_name-speaking_name-default_value-accepted_values
 params = {
     ('mission_configuration', 'Mission Configuration'): [
-        ['select', 'text', 'architecture', 'Architecture / Mission profile', 'upperstage', ['launch_vehicle', 'upperstage']],
+        ['select', 'text', 'architecture', 'Architecture / Mission profile', 'upperstage', ['upperstage']],
         ['select', 'text', 'propulsion_type', 'Propulsion type', 'electrical', ['electrical', 'water', 'mono-propellant', 'solid', 'bi-propellant']],
         ['select', 'text', 'deployment_strategy', 'Deployment strategy', 'one_plane_at_a_time_sequential', ['one_plane_at_a_time_sequential', 'one_sat_per_plane_sequential']],
         ['checkbox', '', 'verbose', 'Debug (display figures)', False, [True, False]],
@@ -10,7 +10,7 @@ params = {
     ('constellation_configuration', 'Constellation Configuration'): [
         ['input', 'text', 'constellation_name', 'Constellation Name', 'My Constellation', '^[A-Za-z0-9 ]+$'],
         ['input', 'number', 'sat_mass', 'Satellite Mass (kg)', 147, [0.1, 99999.9]],
-        ['input', 'number', 'sat_volume', 'Satellite Volume (m^3)', 0, [0, 99999.9]],
+        ['input', 'number', 'sat_volume', 'Satellite Volume (m^3)', None, [0.001, 99999.9]],
         ['input', 'number', 'n_planes', 'Number of orbital planes', 3, [1, 100]],
         ['input', 'number', 'n_sats_per_plane', 'Number of satellite per orbital plane', 2, [1, 1000]],
         ['input', 'number', 'plane_distribution_angle', 'Distribution angle of orbital planes (deg)', 180, [180, 360]],
