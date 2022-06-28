b02696d961664347a4445494bf21019c929f181f
malogoury-tcdc <102596835+malogoury-tcdc@users.noreply.github.com>
2022-06-28 10:18:02 +0200

    create inhetited ADR and Deployment constellations

---
+++ Constellations/ConstellationForDeployment.py
@@ -0,0 +1,12 @@
+"""
+Created:        ?
+Last Revision:  23.05.2022
+Author:         ?,Emilien Mingard
+Description:    Constellation,Satellite Classes definitions
+"""
+# Import Classes
+from Constellations.Constellation import *
+
+class Constellation(Constellation):
+    def __init__(self, constellation_id):
+        super().__init__(constellation_id)
