a11853f5db307f0e4cb7e9efc68a64ce65c433fc
malogoury-tcdc <102596835+malogoury-tcdc@users.noreply.github.com>
2022-06-28 12:34:38 +0200

    ScenarioADR: create class

--- Scenarios/ScenarioADR.py
+++ Scenarios/ScenarioADR.py
@@ -1,6 +1,18 @@
 """
-Created:        17.05.2022
-Last Revision:  -
-Author:         Emilien Mingard
-Description:    ADR dedicated Scenario Class
+Created:        28.06.2022
+Last Revision:  28.06.2022
+Author:         Malo Goury
+Description:    Scenario for constellation deployment
 """
+
+# Import Class
+from Scenarios.Scenario import *
+from Constellations.ConstellationForADR import ConstellationForADR
+
+# Class definition
+class ScenarioADR(Scenario):
+    def __init__(self, scenario_id, json):
+        super().__init__(scenario_id, json)
+
+    def create_constellation(self):
+        self.constellation = ConstellationForADR(self.constellation_name)
