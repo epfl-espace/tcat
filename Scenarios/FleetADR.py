73611db90e32a9539ba474d6f33d039493220277
EmiTCDC <101722438+EmiTCDC@users.noreply.github.com>
2022-06-28 15:23:34 +0200

    FleetADR: first release

--- Scenarios/FleetADR.py
+++ Scenarios/FleetADR.py
@@ -7,6 +7,8 @@ Description:    FleetADR related class
 
 # Import Classes
 from Scenarios.Fleet import Fleet
+from Spacecrafts.Servicer import Servicer
+from Spacecrafts.UpperStage import UpperStage
 
 # Import libraries
 import warnings
@@ -26,6 +28,52 @@ class FleetADR(Fleet):
 
         # Dictionnaries of servicers and upperstages
         self.servicers = dict()
+
+    """
+    Methods
+    """
+    def execute(self,clients):
+        """ This function calls all appropriate methods to design the fleet to perform a particular plan.
+
+        Args:
+            clients (Constellation): full or tailored constellation containing satellite treated as targets to reach by any spacecraft
+            verbose (boolean): if True, print convergence information
+        """
+        # Retrieve unassigned broken satellites
+        unassigned_satellites = clients.get_optimized_ordered_satellites()
+
+        # Spacecraft launcher counter
+        spacecraft_count = 0
+
+        # Strategy depend on architecture
+        if self.scenario.architecture == "single_picker":
+            for unassigned_satellite in unassigned_satellites:
+                # Create Servicer and compute servicer plan
+                servicer = Servicer(f"Servicer_{spacecraft_count:04d}",self.scenario,mass_contingency=0.0)
+                servicer.execute(unassigned_satellite)
+
+                # Create UpperStage
+                spacecraft_count += 1
+                upperstage = UpperStage(f"UpperStage_{spacecraft_count:04d}",self.scenario,mass_contingency=0.0)
+                upperstage.execute(servicer,constellation_precession=0,custom_sat_allowance=1) # No RAAN margin and a single servicer per upperstage
+
+                # Add converged UpperStage
+                self.add_servicer(servicer)
+
+                # Add converged UpperStage
+                self.add_upperstage(upperstage)
+                
+                # Remove latest assigned satellites
+                clients.remove_in_ordered_satellites(servicer.get_ordered_target_spacecraft())
+                
+                # Check remaining satellites to be assigned
+                unassigned_satellites = clients.get_optimized_ordered_satellites()
+
+                # Update execution counter
+                execution_count += 1
+            
+        elif self.scenario.architecture == "multi_picker":
+            raise Exception('multi_picker option not available')
 
     def add_servicer(self, servicer):
         """ Adds a servicer to the Fleet class.
