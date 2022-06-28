ce959a7bee32b0d6c1b3596a03fb961dc5a39ff7
malogoury-tcdc <102596835+malogoury-tcdc@users.noreply.github.com>
2022-06-28 12:34:13 +0200

    ScenarioConstellation: use directly json as input

--- Scenarios/ScenarioConstellation.py
+++ Scenarios/ScenarioConstellation.py
@@ -1,8 +1,8 @@
 """
-Created:        17.05.2022
+Created:        28.06.2022
 Last Revision:  28.06.2022
-Author:         Emilien Mingard
-Description:    Constellation dedicated Scenario Class definition
+Author:         Malo Goury
+Description:    Scenario for constellation deployment
 """
 
 # Import Class
@@ -11,8 +11,8 @@ from Constellations.ConstellationForDeployment import ConstellationForDeployment
 
 # Class definition
 class ScenarioConstellation(Scenario):
-    def __init__(self, scenario_id, config_file):
-        super().__init__(scenario_id, config_file)
+    def __init__(self, scenario_id, json):
+        super().__init__(scenario_id, json)
 
     def create_constellation(self):
         self.constellation = ConstellationForDeployment(self.constellation_name)
