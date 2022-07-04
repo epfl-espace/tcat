613c65d0e30d11e79440303f48b89283d69859b1
EmiTCDC <101722438+EmiTCDC@users.noreply.github.com>
2022-07-04 14:07:09 +0200

    Satellite: comments

--- Spacecrafts/Satellite.py
+++ Spacecrafts/Satellite.py
@@ -3,31 +3,33 @@ from Phases.Common_functions import nodal_precession
 
 
 class Satellite(Spacecraft):
-    """ Satellite consist of an object in an initial orbit that can be moved by servicers.
-    The class is initialized by giving the current object mass and orbit at time 0.
-    It is added to the Clients class taken as argument during initialization.
+    """ Satellite acts as a child Class to describe passive satellite such as constellation satellites.
 
-    Args:
-        satellite_id (str): Standard id. Needs to be unique.
-        initial_mass (u.kg): Object mass at time 0.
-        insertion_orbit (poliastro.twobody.Orbit): Object orbit after insertion
-        operational_orbit (poliastro.twobody.Orbit): Object orbit after orbit raising
-        state (str): descriptor of the satellite state, used to identify different possible failures and states
-
-    Attributes:
-        id (str): Standard id. Needs to be unique.
-        initial_mass (u.kg): Object mass at time 0.
-        volume (u.m^3): Volume of the satellite
-        initial_orbit (poliastro.twobody.Orbit): Initial orbit of the satellite
-        insertion_orbit (poliastro.twobody.Orbit): Object orbit after insertion
-        operational_orbit (poliastro.twobody.Orbit): Object orbit after orbit raising
-        current_orbit (poliastro.twobody.Orbit): Object orbit at current time.
-        current_mass (u.kg): Object mass at current time.
-        state (str): descriptor of the satellite state, used to identify different possible failures and states
+    :param satellite_id: Satellite identification name
+    :type satellite_id: str
+    :param initial_mass: Satellite initial mass
+    :type initial_mass: u*kg
+    :param volume: Satellite volume
+    :type volume: u*m**3
+    :param insertion_orbit: Insertion orbit
+    :type insertion_orbit: poliastro.twobody.Orbit
+    :param operational_orbit: Operational orbit
+    :type operational_orbit: poliastro.twobody.Orbit
+    :param disposal_orbit: Disposal orbit
+    :type disposal_orbit: poliastro.twobody.Orbit
+    :param state: Spacecraft actual state
+    :type state: str
+    :param default_orbit: Initial orbit
+    :type default_orbit: poliastro.twobody.Orbit
     """
-    def __init__(self, satellite_id, initial_mass, volume, insertion_orbit=None, operational_orbit=None, disposal_orbit=None, state="standby", default_orbit=None, is_stackable=False):
+    def __init__(self, satellite_id, initial_mass, volume, insertion_orbit=None, operational_orbit=None, disposal_orbit=None, state="standby", default_orbit=None):
         super().__init__(satellite_id, initial_mass, volume,insertion_orbit=insertion_orbit,operational_orbit=operational_orbit, disposal_orbit=disposal_orbit,state=state)
         self.default_orbit = default_orbit
 
     def get_default_orbit(self):
+        """ Get the satellite default orbit
+
+        :return: default orbit
+        :rtype: poliastro.twobody.Orbit
+        """
         return self.default_orbit
