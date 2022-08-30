3f2cfe8c0cc1d8fda7f3d5568a1bdb3fd18c7033
malogoury-tcdc <102596835+malogoury-tcdc@users.noreply.github.com>
2022-08-30 16:54:28 +0200

    Fleeet: use encapsulation for printing number of spacecraft

--- Fleets/Fleet.py
+++ Fleets/Fleet.py
@@ -483,18 +483,21 @@ class Fleet:
         print(f"Mission duration: {convert_time_for_print(duration):.2f}")
         print("")
 
-        # Number of launcher
-        Nb_UpperStage = len(self.upperstages)
-        if Nb_UpperStage > 1:
-            print(f"UpperStages: {Nb_UpperStage}")
-        else:
-            print(f"UpperStage: {Nb_UpperStage}")
+        self.print_nb_fleet_spacecraft()
 
         # Print total launcher mass accros the fleet
         launchers_mass = [self.upperstages[key].get_initial_wet_mass() for key in self.upperstages.keys()]
         print(F"Total mass launched in space: {sum(launchers_mass):.2f}")
         payload_mass = [upperstage.get_initial_payload_mass() for upperstage in self.upperstages.values()]
         print(f"Total payload mass released in space: {sum(payload_mass):.2f}")
+
+    def print_nb_fleet_spacecraft(self):
+        # Number of launcher
+        Nb_UpperStage = len(self.upperstages)
+        if Nb_UpperStage > 1:
+            print(f"UpperStages: {Nb_UpperStage}")
+        else:
+            print(f"UpperStage: {Nb_UpperStage}")
 
 
     def __str__(self):
