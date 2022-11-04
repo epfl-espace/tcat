30670d55338754838428f6d9bb6cefaaf18394aa
malogoury-tcdc <102596835+malogoury-tcdc@users.noreply.github.com>
2022-11-04 17:53:32 +0100

    ACTLinker: Add parameters getter

--- ScenarioDatabase/act_config_linker.py
+++ ScenarioDatabase/act_config_linker.py
@@ -1,9 +1,7 @@
 import json
 
-from numpy import empty
-
 BB_ID_KICKSTAGE = "b02a2165-7e7c-4473-8e5c-1d7ca50dfa3e"
-KICKSTAGE_PARAM_ID_DIAM = "527f86c2-e3f5-479b-aff4-1d304c7a4cbb"
+PARAM_ID_KICKSTAGE_DIAMETER = "527f86c2-e3f5-479b-aff4-1d304c7a4cbb"
 
 class ACTConfigLinker:
     def __init__(self,json_filepath=None):
@@ -36,25 +34,55 @@ class ACTConfigLinker:
                 return buildingblock
         return None
 
+    def get_buildingblock_parameters(self,config_name,buildingblock_id):
+        buildingblock = self.get_buildingblock(config_name,buildingblock_id)
+        if buildingblock is None:
+            return None
+        return buildingblock["parameters"]
+
+    def get_buildingblock_parameter(self,config_name,buildingblock_id,param_type_id):
+        bb_params = self.get_buildingblock_parameters(config_name,buildingblock_id)
+        if bb_params is None: 
+            return None
+
+        for param in bb_params:
+            if "type" in param:
+                if param["type"]["id"] == param_type_id:
+                    return param
+        return None
+
+    def get_buildingblock_parameter_value(self,config_name,buildingblock_id,param_type_id):
+        bb_param = self.get_buildingblock_parameter(config_name,buildingblock_id,param_type_id)
+        if bb_param is None: 
+            return None
+        return bb_param["value"]
+
+    def get_buildingblock_parameter_unit(self,config_name,buildingblock_id,param_type_id):
+        bb_param = self.get_buildingblock_parameter(config_name,buildingblock_id,param_type_id)
+        if bb_param is None: 
+            return None
+        if "type" in bb_param:
+            if "unit" in bb_param["type"]:
+                return bb_param["type"]["unit"]["name"]
+        return None
+
     def get_buildingblock_kickstage(self,config_name):
         return self.get_buildingblock(config_name,BB_ID_KICKSTAGE)
 
-    def get_prameter_value(self,param_list,param_type_id):
-        for param in param_list:
-            if param["type"]["id"] == param_type_id:
-                return param["value"]
-        return None
-
-    def get_kickstage_height(self, config_name):
-        config_dict = self.get_configs_dict(config_name)
-        if config_dict is None:
-            return None
-        
+    def get_kickstage_diameter(self, config_name):
+        return self.get_buildingblock_parameter_value(config_name,BB_ID_KICKSTAGE,PARAM_ID_KICKSTAGE_DIAMETER)      
 
 if __name__ == "__main__":
     config_name = "test_tcat_1"
     act = ACTConfigLinker("ScenarioDatabase/Configurations.json")
     configs = act.get_configs_name()
+
     bb = act.get_buildingblock(config_name,BB_ID_KICKSTAGE)
+    bb_params = act.get_buildingblock_parameters(config_name,BB_ID_KICKSTAGE)
+    bb_param = act.get_buildingblock_parameter(config_name,BB_ID_KICKSTAGE,PARAM_ID_KICKSTAGE_DIAMETER)
+    bb_param_val = act.get_buildingblock_parameter_value(config_name,BB_ID_KICKSTAGE,PARAM_ID_KICKSTAGE_DIAMETER)
+    bb_param_u = act.get_buildingblock_parameter_unit(config_name,BB_ID_KICKSTAGE,PARAM_ID_KICKSTAGE_DIAMETER)
+
     bb_kick = act.get_buildingblock_kickstage(config_name)
+    kick_diameter = act.get_kickstage_diameter(config_name)
     pass
