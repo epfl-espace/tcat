29741923023dc40e04f1402677d8bd6e07d19bad
malogoury-tcdc <102596835+malogoury-tcdc@users.noreply.github.com>
2022-11-05 21:10:27 +0100

    TestACTLinker: adapted to new structure

--- TestACTLinker.py
+++ TestACTLinker.py
@@ -1,22 +1,13 @@
-from ScenarioDatabase.ACTConfigLinker.ACTConfigLinker import ACTConfigLinker
 from ScenarioDatabase.ACTConfigLinker.ACTConfigIDs import *
 
-from ScenarioDatabase.ScenarioSetupFromACT import ScenarioSetupFromACT
+from ScenarioDatabase.ScenariosSetupFromACT.ScenarioADRSetupFromACT import ScenarioADRSetupFromACT
 
 config_name = "test_tcat_1"
-act = ACTConfigLinker("ScenarioDatabase/Configurations.json")
+a2t = ScenarioADRSetupFromACT()
+a2t.open_act_config_json("ScenarioDatabase/Configurations.json")
 
-bb = act.get_buildingblock(config_name,BB_ID_KICKSTAGE)
-bb_params = act.get_buildingblock_parameters(config_name,BB_ID_KICKSTAGE)
-bb_param = act.get_buildingblock_parameter(config_name,BB_ID_KICKSTAGE,PARAM_ID_KICKSTAGE_DIAMETER)
-bb_param_val = act.get_buildingblock_parameter_value(config_name,BB_ID_KICKSTAGE,PARAM_ID_KICKSTAGE_DIAMETER)
-bb_param_u = act.get_buildingblock_parameter_unit(config_name,BB_ID_KICKSTAGE,PARAM_ID_KICKSTAGE_DIAMETER)
 
-configs = act.get_configs_name()
-# kick_bb = act.get_buildingblock_kickstage(config_name)
-# kick_diameter = act.get_kickstage_diameter(config_name)
-
-si = ScenarioSetupFromACT()
-si.to_json_file("./my_new_json.json")
+a2t.read_act_config(config_name)
+a2t.export_adr_config_to_json("./my_new_json.json")
 
 pass
