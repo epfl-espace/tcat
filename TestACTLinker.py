9c59c2689e70fe13cbf773b370053532abffdccf
malogoury-tcdc <102596835+malogoury-tcdc@users.noreply.github.com>
2022-11-07 15:28:09 +0100

    Update TestACTLinker.py

--- TestACTLinker.py
+++ TestACTLinker.py
@@ -1,12 +1,8 @@
-from ScenarioDatabase.ACTConfigLinker.ACTConfigIDs import *
-
 from ScenarioDatabase.ScenariosSetupFromACT.ScenarioADRSetupFromACT import ScenarioADRSetupFromACT
 
 config_name = "test_tcat_1"
 a2t = ScenarioADRSetupFromACT()
 a2t.open_act_config_json("ScenarioDatabase/Configurations.json")
-
-
 a2t.read_act_config(config_name)
 a2t.export_adr_config_to_json("./my_new_json.json")
 
