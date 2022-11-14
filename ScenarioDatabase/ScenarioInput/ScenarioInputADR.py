8d35780f02b2eac4f54131a79332758fc5635d19
malogoury-tcdc <102596835+malogoury-tcdc@users.noreply.github.com>
2022-11-14 15:10:39 +0100

    ScenarioInput: added units for all parameters

--- ScenarioDatabase/ScenarioInput/ScenarioInputADR.py
+++ ScenarioDatabase/ScenarioInput/ScenarioInputADR.py
@@ -9,29 +9,29 @@ class ScenarioInputADR(ScenarioInputBase):
     mission_architecture: str = "single_picker"
 
     # Constellation parameters
-    sats_reliability: float = 0.5
+    sats_reliability: float = 0.5 # [0-1]
     seed_random_sats_failure: int = 1234
 
     # Servicer parameters
-    servicer_initial_fuel_mass: float = 100.0
-    servicer_capture_module_dry_mass: float = 10.0
-    servicer_prop_thrust: float = 29400.0
-    servicer_prop_isp: float = 330.0
-    servicer_propulsion_dry_mass: float = 20.0
-    servicer_struct_mass: float = 10.0
-    servicer_default_volume: float = 2.0
+    servicer_initial_fuel_mass: float = 100.0 # kg
+    servicer_capture_module_dry_mass: float = 10.0 # kg
+    servicer_prop_thrust: float = 29400.0 # N
+    servicer_prop_isp: float = 330.0 # s
+    servicer_propulsion_dry_mass: float = 20.0 # kg
+    servicer_struct_mass: float = 10.0 # kg
+    servicer_default_volume: float = 2.0 # m3
     servicer_propulsion_type: str = "bi-propellant"
 
     # Orbital parameters 
-    apogee_sats_operational: float = 1200.0
-    perigee_sats_operational: float = 1200.0
-    inc_sats_operational: float = 87.4
-    apogee_sats_disposal: float = 1500.0
-    perigee_sats_disposal: float = 1500.0
-    inc_sats_disposal: float = 87.4
-    apogee_servicer_disposal: float = 1800.0
-    perigee_servicer_disposal: float = 1800.0
-    inc_servicer_disposal: float = 85.0
+    apogee_sats_operational: float = 1200.0 # km (altitude)
+    perigee_sats_operational: float = 1200.0 # km (altitude)
+    inc_sats_operational: float = 87.4 # deg
+    apogee_sats_disposal: float = 1500.0 # km (altitude)
+    perigee_sats_disposal: float = 1500.0 # km (altitude)
+    inc_sats_disposal: float = 87.4 # deg
+    apogee_servicer_disposal: float = 1800.0 # km (altitude)
+    perigee_servicer_disposal: float = 1800.0 # km (altitude)
+    inc_servicer_disposal: float = 85.0 # deg
 
     def get_adr_config_as_json(self):
         return self.to_json()
--- ScenarioDatabase/ScenarioInput/ScenarioInputBase.py
+++ ScenarioDatabase/ScenarioInput/ScenarioInputBase.py
@@ -10,15 +10,15 @@ class ScenarioInputBase:
     verbose: bool = False
     starting_epoch: str = "2025-01-01 12:00:00"
     dir_path_for_output_files: str = "./Results"
-    tradeoff_mission_price_vs_duration: float = 0.1
+    tradeoff_mission_price_vs_duration: float = 0.1 # [0.0-1.0]
 
     # Constellation parameters
     constellation_name: str = "OneWeb"
-    sat_mass: float = 147.0
-    sat_volume: float = 1.3
+    sat_mass: float = 147.0 # kg
+    sat_volume: float = 1.3 # m3
     n_planes: int = 3
     n_sats_per_plane: int = 2
-    plane_distribution_angle: float = 180.0
+    plane_distribution_angle: float = 180.0 # deg
 
     # Launcher parameters
     launcher_use_database: bool = False
@@ -26,32 +26,32 @@ class ScenarioInputBase:
     launcher_launch_site: str = "Baikonur"
     launcher_orbit_type: str = "LEO"
     launcher_perf_interpolation_method: str = "linear"
-    launcher_performance: float = 5000.0
-    launcher_fairing_diameter: float = 3.800
-    launcher_fairing_cylinder_height: float = 5.070
-    launcher_fairing_total_height: float = 9.518
+    launcher_performance: float = 5000.0 # kg
+    launcher_fairing_diameter: float = 3.800 # m
+    launcher_fairing_cylinder_height: float = 5.070 # m
+    launcher_fairing_total_height: float = 9.518 # m
 
     # Kickstage parameters
     kickstage_use_database: bool = False
     kickstage_name: str = "Fregat"
-    kickstage_height: float = 1.5
-    kickstage_diameter: float = 2.0
-    kickstage_initial_fuel_mass: float = 500.0
-    kickstage_prop_thrust: float = 294000.0
-    kickstage_prop_isp: float = 330.0
-    kickstage_propulsion_dry_mass: float = 5.0
-    kickstage_dispenser_dry_mass: float = 93.3
-    kickstage_struct_mass: float = 10.0
+    kickstage_height: float = 1.5 # m
+    kickstage_diameter: float = 2.0 # m
+    kickstage_initial_fuel_mass: float = 500.0 # kg
+    kickstage_prop_thrust: float = 294000.0 # N
+    kickstage_prop_isp: float = 330.0 # s
+    kickstage_propulsion_dry_mass: float = 5.0 # m
+    kickstage_dispenser_dry_mass: float = 93.3 # m
+    kickstage_struct_mass: float = 10.0 # kg
     kickstage_propulsion_type: str = "bi-propellant"
-    kickstage_remaining_fuel_margin: float = 0.0
+    kickstage_remaining_fuel_margin: float = 0.0 # kg
 
     # Orbital parameters               
-    apogee_launcher_insertion: float = 400.0
-    perigee_launcher_insertion: float = 400.0
-    inc_launcher_insertion: float = 87.0
-    apogee_launcher_disposal: float = 400.0
-    perigee_launcher_disposal: float = 50.0
-    inc_launcher_disposal: float = 85.0
+    apogee_launcher_insertion: float = 400.0 # km (altitude)
+    perigee_launcher_insertion: float = 400.0 # km (altitude)
+    inc_launcher_insertion: float = 87.0 # deg
+    apogee_launcher_disposal: float = 400.0 # km (altitude)
+    perigee_launcher_disposal: float = 50.0 # km (altitude)
+    inc_launcher_disposal: float = 85.0 # deg
 
     def to_json(self):
         return json.dumps(asdict(self))
